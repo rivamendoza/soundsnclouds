a4a04487753e71a817ecbc57628dce46a766dda7
rivamendoza <riva.mendoza@hotmail.com>
2020-09-14 22:19:21 +1000

    i lie this is submission #1

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,8 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  let host = req.get('host').split(":")[0];
-  res.render('index', { title: 'soundsnclouds server running on http://' + host});
+  res.render('index', { title: 'soundsnclouds server'});
 });
 
 module.exports = router;
